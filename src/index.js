356bc0d111751bbbcfa9045f7a10bff3e02c8d02
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-05-09 22:56:04 +0200

    a

--- src/index.js
+++ src/index.js
@@ -9,55 +9,27 @@ let pageCounter;
 console.log('wyszukiwarka obrazów');
 const inputEl = document.querySelector('.search-input');
 const submitBtn = document.querySelector('.submit-btn');
-// const loadMoreBtn = document.querySelector('.load-more');
 const galleryEl = document.querySelector('.gallery');
 
-const fetchFirstData = async phraseToSearch => {
-  const response = await fetch(
-    `https://pixabay.com/api/?key=35826418-2b05267c07a6de2b6b5980d0a&q=${phraseToSearch}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  const responseJSON = await response.json();
-  Notiflix.Notify.info(
-    `Hooray! We found ${responseJSON.totalHits} images.`
-  )
-    // .catch(error => console.log('error : ', error));
-};
-
 const fetchData = async phraseToSearch => {
-  const response = await fetch(
+  const response = await axios.get(
     `https://pixabay.com/api/?key=35826418-2b05267c07a6de2b6b5980d0a&q=${phraseToSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageCounter}`
   );
-  const responseJSON = await response.json();
-  const data = await responseJSON.hits;
-  //  data.catch( error => console.log('error : ', error));
-  // .then(response => response.json())
-  // .then(data => data.hits)
-  // .catch(error => console.log('error : ', error));
-
-  return data;
+  if (response.data.totalHits > 0 && pageCounter === 1) {
+    Notiflix.Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
+  }
+  return response.data.hits;
 };
 
-// const fetchData = async phraseToSearch => {
-//   return await axios.get('https://pixabay.com/api/', {
-//       params: {
-//         key: `35826418-2b05267c07a6de2b6b5980d0a`,
-//         q: `${inputEl.value}`,
-//         image_type: `photo`,
-//         orientation: `horizontal`,
-//         safesearch: true,
-//       },
-//     })
-//     .then(response => response.json())
-//     .then(data => data.hits)
-//     .catch(error => console.log('error : ', error));
-// };
-
 const renderGalleryItems = pictures => {
-  if (pictures.length < 40) {
+  if (pictures.length < 40 && pictures.length !== 0) {
     Notiflix.Notify.failure(
       "We're sorry, but you've reached the end of search results."
     );
-    // loadMoreBtn.style.display = 'none';
+  } else if (pictures.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
   }
 
   pictures.forEach(
@@ -94,35 +66,23 @@ const handleInfiniteScroll = () => {
 
   if (endOfPage) {
     pageCounter++;
-    fetchData(inputEl.value).then(res => {
-      renderGalleryItems(res);
-    });
+    fetchData(inputEl.value)
+      .then(res => {
+        renderGalleryItems(res);
+      })
+      .catch(error => console.error(error));
+    
   }
 };
 
 submitBtn.addEventListener('click', e => {
-  fetchFirstData(inputEl.value);
   pageCounter = 1;
-  // loadMoreBtn.style.display = 'block';
   galleryEl.innerHTML = '';
   e.preventDefault();
   fetchData(inputEl.value).then(res => {
-    console.log(res);
     renderGalleryItems(res);
   });
 });
-
-// loadMoreBtn.addEventListener('click', () => {
-//   pageCounter++;
-//   fetchData(inputEl.value).then(res => {
-//     console.log(res);
-//     renderGalleryItems(res);
-//   });
-// });
-
-// galleryEl.addEventListener('click', e => {
-//   e.preventDefault();
-// });
 
 window.addEventListener(
   'scroll',
@@ -131,6 +91,4 @@ window.addEventListener(
   // }, 1000)
 );
 
-// var gallery = $('.gallery a').simpleLightbox();
 var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 500 });
-console.log('x')
